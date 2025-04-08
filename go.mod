9cfa020ff73e70cdde242fcafb960cf8a63c213a
ayawen <cloudstlf136@gmail.com>
2025-04-08 10:18:44 +0800

    add mathutil package

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/SFLwen/vuemusic
---
+++ mathutil/mathutil.go
@@ -0,0 +1,11 @@
+package mathutil
+
+//这个包会被上传到github
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Multiply(a, b int) int {
+	return a * b
+}
